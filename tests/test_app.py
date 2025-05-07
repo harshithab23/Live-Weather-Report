7699b767129f6e754dfd5f0bdefa8b29a9af08b6
Harshitha B <bharshitha024@gmail.com>
2025-05-07 10:47:22 +0530

    pylint for linting and unittest (pyunit) for testing

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+# tests/test_app.py
+import unittest
+from app import app
+
+class TestWeatherApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
